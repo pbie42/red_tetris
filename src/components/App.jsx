fa7c358bf143bd2ff9636416cdb762adb5b2e8a6
Paul BIE <pbie@e1r7p5.42.fr>
2019-02-08 17:04:10 +0100

    [Add] - Airbnb eslint, absolute import paths, practice tests.

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import CommentList from './CommentList';
+import CommentBox from './CommentBox';
+
+export default () => (
+  <div>
+    <CommentBox />
+    <CommentList />
+  </div>
+);
