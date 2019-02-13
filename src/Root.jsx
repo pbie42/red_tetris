c5332ee83aad1b9940a395afe7f620483e6fa359
Paul BIE <pbie@e1r7p6.42.fr>
2019-02-13 14:01:34 +0100

    [Add] - Redux state validator middleware

--- src/Root.jsx
+++ src/Root.jsx
@@ -4,10 +4,11 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 // import reduxPromise from 'redux-promise';
 import async from 'middlewares/async';
+import stateValidator from 'middlewares/stateValidator';
 import reducers from 'reducers';
 
 const Root = ({ initialState = {}, children }) => {
-  const store = createStore(reducers, initialState, applyMiddleware(async));
+  const store = createStore(reducers, initialState, applyMiddleware(async, stateValidator));
   return <Provider store={store}>{children}</Provider>;
 };
 
---
+++ src/middlewares/stateValidator.js
@@ -0,0 +1,8 @@
+import tv4 from 'tv4';
+import stateSchema from 'middlewares/stateSchema';
+
+export default ({ getState }) => next => (action) => {
+  next(action);
+
+  if (!tv4.validate(getState(), stateSchema)) console.log('Schema Error:', tv4.error);
+};
