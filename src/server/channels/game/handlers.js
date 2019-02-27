61575bbd12e8f508291dadf19158e86d98447ca4
Paul BIE <pbie@e1r7p2.42.fr>
2019-02-27 18:57:07 +0100

    [Refactor] - handlePlayerLeave on server side

--- src/server/channels/game/handlers.js
+++ src/server/channels/game/handlers.js
@@ -50,29 +50,40 @@ function handleQueuerLeave(io, socket, game, playerID) {
   gameQueueUpdateEmit(io, game.getId(), game.getQueue());
 }
 
-function handlePlayerLeave(io, socket, games, game, playerID) {
-  let updatedGames = games;
+function removePlayer(socket, game, playerID) {
   game.removePlayer(playerID);
   socket.leave(game.getId());
+  gameResetSocketEmit(socket);
+}
+
+function removeGame(io, games, gameID) {
+  const updatedGames = games.filter(g => g.getId() !== gameID);
+  lobbyUpdateGamesEmit(updatedGames, io);
+  return updatedGames;
+}
+
+function movePlayerFromQueue(io, game) {
+  const queue = game.getQueue();
+  const playerQueued = queue.shift();
+  game.addPlayer(playerQueued);
+  gameQueueUpdateEmit(io, game.getId(), queue);
+}
+
+function setNewLeader(io, game) {
+  game.setNewLeader();
+  gameNewLeaderEmit(io, game.getId(), game.getLeader());
+}
+
+function handlePlayerLeave(io, socket, games, game, playerID) {
+  const updatedGames = games;
+  const gameID = game.getId();
+  removePlayer(socket, game, playerID);
   const queue = game.getQueue();
   const playerCount = game.getPlayersCount();
-  if (playerCount === 0 && queue.length === 0) {
-    updatedGames = updatedGames.filter(g => g.getId() !== game.getId());
-    gameResetSocketEmit(socket);
-    lobbyUpdateGamesEmit(updatedGames, io);
-    return updatedGames;
-  }
-  if (playerCount < 5 && queue.length > 0) {
-    const playerQueued = queue.shift();
-    game.addPlayer(playerQueued);
-    gameQueueUpdateEmit(io, game.getId(), game.getQueue());
-  }
-  if (playerID === game.getLeader()) {
-    game.setNewLeader();
-    gameNewLeaderEmit(io, game.getId(), game.getLeader());
-  }
-  gamePlayersUpdateEmit(io, game.getId(), game.getPlayers());
-  gameResetSocketEmit(socket);
+  if (playerCount === 0 && queue.length === 0) return removeGame(io, games, gameID);
+  if (playerCount < 5 && queue.length > 0) movePlayerFromQueue(io, game);
+  if (playerID === game.getLeader()) setNewLeader(io, game);
+  gamePlayersUpdateEmit(io, gameID, game.getPlayers());
   lobbyUpdateGamesEmit(updatedGames, io);
   return updatedGames;
 }
