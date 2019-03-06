b524d5887cdf5903f36ba28c3b8826113036ea57
Paul BIE <pbie@e2r4p22.42.fr>
2019-03-06 10:51:13 +0100

    [Refactor] - Reorganized files and moved newDisplayBoardWithPiece to utils file in movement folder

---
+++ src/server/channels/game/movement/utils.js
@@ -0,0 +1,24 @@
+function newDisplayBoardWithPiece(piece, playerBoard) {
+  const newDisplayBoard = JSON.parse(JSON.stringify(playerBoard));
+  const { location, shape, letter } = piece.getInfo();
+  let y = 0;
+  let bY = location.y;
+  while (shape[y] && newDisplayBoard[bY]) {
+    let x = 0;
+    let bX = location.x;
+    while (x < shape[0].length) {
+      if (newDisplayBoard[bY] && newDisplayBoard[bY][bX] === 0 && shape[y][x] === letter) {
+        newDisplayBoard[bY][bX] = letter;
+      }
+      bX += 1;
+      x += 1;
+    }
+    bY += 1;
+    y += 1;
+  }
+  return newDisplayBoard;
+}
+
+module.exports = {
+  newDisplayBoardWithPiece,
+};
--- test/server/channels/game/movement/utils.test.js
+++ test/server/channels/game/movement/utils.test.js
@@ -1,12 +1,13 @@
 import Piece from 'server/classes/Piece';
-import { newDisplayBoardWithPiece } from 'server/channels/game/handlers/handleFirstPiece';
+import Player from 'server/classes/Player';
+import { newDisplayBoardWithPiece } from 'server/channels/game/movement/utils';
 import newBoard from 'server/classes/utils/newBoard';
 
 describe('newDisplayBoardWithPiece', () => {
-  const board = newBoard();
+  const player = new Player('1', 'paul');
   const pieceL = new Piece('l', 0);
+  const newDisplayBoard = newDisplayBoardWithPiece(pieceL, player.getBoard());
   it('should place a piece onto a the board passed to it and return a new board with the piece on it', () => {
-    const newDisplayBoard = newDisplayBoardWithPiece(pieceL, board);
     expect(newDisplayBoard).toEqual([
       [0, 0, 0, 0, 0, 'l', 0, 0, 0, 0],
       [0, 0, 0, 'l', 'l', 'l', 0, 0, 0, 0],
@@ -29,5 +30,8 @@ describe('newDisplayBoardWithPiece', () => {
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     ]);
+  });
+  it('should not modify the player board', () => {
+    expect(player.getBoard()).toEqual(newBoard());
   });
 });
