75da501ba6538a218c8808f06186f4eb80c3d276
Paul BIE <pbie@e1r8p6.42.fr>
2019-04-24 14:12:11 +0200

    [Refactor] - Removal of unnecessary console logs

--- src/client/reducers/game.js
+++ src/client/reducers/game.js
@@ -15,8 +15,6 @@ export const gameInitialState = {
 export default (state = gameInitialState, { type, payload }) => {
   switch (type) {
     case types.GAME_SET:
-      console.log('reducer gameset');
-
       return {
         ...state,
         error: '',
@@ -29,49 +27,39 @@ export default (state = gameInitialState, { type, payload }) => {
       };
 
     case types.GAME_SET_ACTIVE:
-      console.log('reducer gamesetactive');
-
       return {
         ...state,
         active: payload.active,
       };
 
     case types.GAME_SET_LISTENING:
-      console.log('reducer gamesetlistening');
-
       return {
         ...state,
         listening: payload.listening,
       };
 
     case types.GAME_SET_NEW_LEADER:
-      console.log('reducer gamesetnewleader');
-
       return {
         ...state,
         leader: payload.leader,
       };
 
     case types.GAME_PLAYERS_UPDATE:
-      console.log('reducer gameplayersupdate');
       return {
         ...state,
         players: payload,
       };
 
     case types.GAME_QUEUE_UPDATE:
-      console.log('reducer gamequeueupdate');
       return {
         ...state,
         queue: payload,
       };
 
     case types.GAME_RESET:
-      console.log('reducer game reset');
       return gameInitialState;
 
     case types.GAME_SET_DIFFICULTY:
-      console.log('Setting difficulty');
       return {
         ...state,
         difficulty: payload.difficulty,
