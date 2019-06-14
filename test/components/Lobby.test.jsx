bd0cb9a4a3a0e53e2c22319900567f70e5ea0791
Paul BIE <pbie@e3r12p5.42.fr>
2019-06-14 13:32:50 +0200

    [Remove] - Skipped tests

--- test/components/Lobby.test.jsx
+++ test/components/Lobby.test.jsx
@@ -1,40 +1,14 @@
-import React from 'react';
-import { mount } from 'enzyme';
-import { MemoryRouter, Redirect } from 'react-router-dom';
 import Lobby from 'client/components/lobby/Lobby';
 import Root from 'client/components/Root';
+import { mount } from 'enzyme';
+import React from 'react';
+import { MemoryRouter, Redirect } from 'react-router-dom';
 
 let wrapped;
 
 describe('The View', () => {
   afterEach(() => {
     wrapped.unmount();
-  });
-
-  xit('has one div and one h1 if username is set', () => {
-    const initialState = {
-      lobby: {
-        games: [],
-      },
-      game: {
-        difficulty: 1,
-        roomName: '',
-      },
-      player: {
-        username: 'Paul',
-        id: '1',
-        error: '',
-      },
-    };
-    wrapped = mount(
-      <Root initialState={initialState}>
-        <MemoryRouter>
-          <Lobby />
-        </MemoryRouter>
-      </Root>,
-    );
-    expect(wrapped.find('div').length).toEqual(1);
-    expect(wrapped.find('h1').length).toEqual(1);
   });
 
   it('redirects to login if no username is set', () => {
@@ -152,44 +126,5 @@ describe('the input', () => {
     wrapped.update();
     expect(wrapped.find('input').prop('value')).toEqual('');
     wrapped.unmount();
-  });
-});
-
-describe('Game Selection', () => {
-  const game1 = {
-    roomName: 'Fun',
-    id: '1',
-    difficulty: 1,
-  };
-  const game2 = {
-    roomName: 'Funner',
-    id: '2',
-    difficulty: 1,
-  };
-  const initialState = {
-    lobby: {
-      games: [game1, game2],
-    },
-    game: {
-      difficulty: 1,
-      roomName: '',
-    },
-    player: {
-      username: 'Paul',
-      id: '1',
-      error: '',
-    },
-  };
-  const gameCreate = jest.fn();
-  xit('should display games that exist in a div with a button for each', () => {
-    wrapped = mount(
-      <Root initialState={initialState}>
-        <MemoryRouter>
-          <Lobby gameCreate={gameCreate} />
-        </MemoryRouter>
-      </Root>,
-    );
-    expect(wrapped.find('.game').length).toEqual(2);
-    expect(wrapped.find('.game-button').length).toEqual(2);
   });
 });
--- test/components/game/Game.test.jsx
+++ test/components/game/Game.test.jsx
@@ -1,50 +1,13 @@
-import React from 'react';
-import { mount } from 'enzyme';
-import { MemoryRouter, Redirect } from 'react-router-dom';
 import Game from 'client/components/game/Game';
-import Player from 'server/classes/Player';
 import Root from 'client/components/Root';
+import { mount } from 'enzyme';
+import React from 'react';
+import { MemoryRouter, Redirect } from 'react-router-dom';
 
 let wrapped;
 
 afterEach(() => {
   wrapped.unmount();
-});
-
-xit('has one h1 if url is valid with player set and game set', () => {
-  const initialState = {
-    player: {
-      id: '1',
-      username: 'Paul',
-      error: '',
-    },
-    game: {
-      queue: [],
-      active: false,
-      difficulty: 1,
-      listening: false,
-      leader: '',
-      roomName: 'Fun',
-      players: [new Player('1', 'Paul')],
-      id: '1',
-    },
-  };
-  const initialMatch = {
-    isExact: true,
-    params: {
-      game: 'testing[Paul]',
-    },
-    path: '/:game',
-    url: '/testing[Paul]',
-  };
-  wrapped = mount(
-    <Root initialState={initialState}>
-      <MemoryRouter>
-        <Game match={initialMatch} />
-      </MemoryRouter>
-    </Root>,
-  );
-  expect(wrapped.find('h1').length).toEqual(1);
 });
 
 it('redirects to lobby if url param is invalid for a game but username is set', () => {
